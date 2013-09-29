6767ee7923f326c89584a4b4f2993ed467dd8522
Roger Steve Ruiz <firstworldman@gmail.com>
2013-09-29 19:22:10 -0400

    Add the source

---
+++ src/js/hero.js
@@ -0,0 +1,8 @@
+define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
+  console.log('jquery version', $.fn.jquery);
+  console.log('underscore', _);
+  console.log('backbone', Backbone);
+  
+  console.log('================================================================================');
+
+});
