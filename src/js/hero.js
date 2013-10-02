133a53ad33a343d6c56cd83f87a35d4145ab8d52
Roger Steve Ruiz <roger.ruiz@rokkan.com>
2013-10-02 15:51:58 -0400

    Randomly show only one <h2> on load for hero

--- src/js/hero.js
+++ src/js/hero.js
@@ -14,6 +14,7 @@ define(function (require) {
     render: function () {
       var $el = $(this.el);
       $el.html(heroTemplate(heroData));
+      $el.find('h2').eq(Math.floor(Math.random() * (3 - 0 + 0) + 0)).addClass('is-active');
     }
   });
 
