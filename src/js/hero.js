19dc156811bf5c455964a7e3ee4fd1106ee98f9f
Roger Steve Ruiz <roger.ruiz@rokkan.com>
2013-10-03 15:53:51 -0400

    Cleanup hero
    + Update the hero data to be simpler
    + Update the template for hero to handle the new data schema
    + Add a Backbone.Model to hero module that sets the hero data
    + Tie the Model to the View
    + Add event parameters to the event methods

--- src/js/hero.js
+++ src/js/hero.js
@@ -7,10 +7,20 @@ define(function (require) {
   var heroTemplate = require('hb!tmp/hero.hbs');
   var heroData = JSON.parse(require('text!data/hero.json'));
 
-  var HeroView = Backbone.View.extend({
+  var HeroModel = Backbone.Model.extend({
     initialize: function () {
-      this.data = heroTemplate(heroData);
-    },
+      this.set({
+        main: heroData.main,
+        github: heroData.github,
+        twitter: heroData.twitter,
+        pinboard: heroData.pinboard
+      });
+    }
+  });
+
+  var HeroView = Backbone.View.extend({
+    initialize: function () {},
+    model: new HeroModel,
     events: {
       'mouseover': 'stopUpdate',
       'mouseout': 'startUpdate',
@@ -18,20 +28,20 @@ define(function (require) {
     },
     el: '#js-hero',
     render: function () {
-      this.$el.html(this.data);
+      this.$el.html(heroTemplate(this.model.attributes));
       this.startUpdate();
       return this;
     },
-    startUpdate: function () {
+    startUpdate: function (evt) {
       var self = this;
       window.heroInterval = setInterval(function () {
         self.update();
       }, 5000);
     },
-    stopUpdate: function () {
+    stopUpdate: function (evt) {
       clearInterval(window.heroInterval);
     },
-    update: function () {
+    update: function (evt) {
       var total = this.$('.hero__title').length;
       var index = this.$('.is-active').index();
       var next = index + 1 < total ? index + 1 : 0;
